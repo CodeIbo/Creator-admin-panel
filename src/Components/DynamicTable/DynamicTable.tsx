7cafbde37a71e937bb9b832447e0c7adb72b8fea
Dawid <ibonomgame@gmail.com>
2024-04-19 09:28:03 +0200

    Update Component Dynamic Table with helper JSON
    Dynamic Table is now render by information provided via JSON

--- src/Components/DynamicTable/DynamicTable.tsx
+++ src/Components/DynamicTable/DynamicTable.tsx
@@ -1,8 +1,3 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
-/* eslint-disable react/no-array-index-key */
-/* eslint-disable react/no-unescaped-entities */
-/* eslint-disable array-callback-return */
-import * as React from 'react';
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
 import TableCell from '@mui/material/TableCell';
@@ -10,30 +5,35 @@ import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
-import { useEffect, useState } from 'react';
-import { Link, useNavigate } from 'react-router-dom';
+import { useState } from 'react';
+import { Link, useSearchParams } from 'react-router-dom';
 import Button from '@mui/material/Button';
 import DeleteIcon from '@mui/icons-material/Delete';
-import DynamicObject from '../../Models/DynamicObject';
-import DataNames from '../../Models/dataNames';
+import { ButtonGroup, Typography } from '@mui/material';
+import _ from 'lodash';
+import { TableDataNames } from '../../Models/dataNames';
 import jsonData from '../../Services/Helpers/tableNames.json';
 import useFetch from '../../Services/Hooks/useFetch';
-import AlertDialog from '../AlertDialog/AlertDialog';
+import LinkButton from '../LinkButton/LinkButton';
+import {
+  ApiCallback,
+  AxiosResponseUnTypedData,
+} from '../../Models/AxiosResponse';
+import { TableHelper } from '../../Models/JSONHelpers';
 
 function DynamicTable({
-  data,
+  data = [],
   dataName,
 }: {
-  data: DynamicObject[];
-  dataName: DataNames;
+  data: AxiosResponseUnTypedData['data'];
+  dataName: TableDataNames;
 }) {
-  const jsonUntypedData = jsonData[dataName] as DynamicObject;
-  const [tableData, setTableData] = useState(data);
-  const navigate = useNavigate();
-  const { response, isLoading, error, apiHandler } = useFetch();
-  const redirectToEditPage = (objectId: string) => {
-    navigate(objectId, { relative: 'path' });
-  };
+  const jsonUntypedData: TableHelper[TableDataNames] = jsonData[dataName];
+  const [tableData, setTableData] = useState<ApiCallback[] | []>(
+    data as ApiCallback[] | []
+  );
+  const { apiHandler } = useFetch();
+  const [searchParams] = useSearchParams();
 
   const deleteHandler = (objectId: string) => {
     apiHandler({
@@ -46,70 +46,149 @@ function DynamicTable({
       })
     );
   };
-
-  return (
-    <TableContainer component={Paper}>
-      <Table sx={{ minWidth: 650 }} aria-label="simple table">
-        <TableHead>
-          <TableRow>
-            {Object.keys(jsonUntypedData).map((str: string) => (
-              <TableCell align="center" key={str}>
-                {jsonUntypedData[str]}
-              </TableCell>
-            ))}
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {tableData.map((singleObject: any, index) => {
-            return (
-              <TableRow
-                key={index}
-                sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-              >
-                {Object.keys(jsonUntypedData).map((key: string, indexJSON) => {
-                  if (singleObject[key]) {
+  return tableData && _.isArray(tableData) && tableData.length > 0 ? (
+    <>
+      <LinkButton
+        variant="contained"
+        sx={{
+          margin: '0 0 1rem auto',
+          display: 'block',
+          maxWidth: '10rem',
+          textAlign: 'center',
+        }}
+        component={Link}
+        to="new"
+        buttonText="ADD NEW"
+        state={{ urlKey: searchParams.get('key') }}
+      />
+      <TableContainer component={Paper}>
+        <Table sx={{ minWidth: 650 }} aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              {Object.keys(jsonUntypedData).map((str) => (
+                <TableCell align="center" key={str}>
+                  {
+                    jsonUntypedData[str as keyof TableHelper[TableDataNames]]
+                      ?.header
+                  }
+                </TableCell>
+              ))}
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {tableData.map((singleObject, index) => {
+              return (
+                <TableRow
+                  key={Object.keys(singleObject)[index]}
+                  sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                >
+                  {Object.keys(jsonUntypedData).map((key) => {
+                    if (key in singleObject) {
+                      return (
+                        <TableCell align="center" key={key}>
+                          {singleObject[key as keyof ApiCallback]}
+                        </TableCell>
+                      );
+                    }
+                    if (key === 'action') {
+                      return (
+                        <TableCell
+                          key={key}
+                          sx={{
+                            display: 'flex',
+                            justifyContent: 'center',
+                            gap: '1rem',
+                          }}
+                        >
+                          <Button
+                            variant="outlined"
+                            component={Link}
+                            to={singleObject.id}
+                          >
+                            Edit
+                          </Button>
+                          <Button
+                            variant="outlined"
+                            color="error"
+                            onClick={() => {
+                              deleteHandler(singleObject.id);
+                            }}
+                          >
+                            DELETE <DeleteIcon />
+                          </Button>
+                        </TableCell>
+                      );
+                    }
+                    if (key === 'children') {
+                      return (
+                        <TableCell sx={{ textAlign: 'center' }} key={key}>
+                          <Link
+                            to={{
+                              pathname: `../${jsonUntypedData[
+                                key as keyof TableHelper[TableDataNames]
+                              ]?.dashboard_url}`,
+                              search: `key=${
+                                singleObject[
+                                  jsonUntypedData[
+                                    key as keyof TableHelper[TableDataNames]
+                                  ]?.key as keyof ApiCallback
+                                ]
+                              }`,
+                            }}
+                            relative="path"
+                          >
+                            {`${jsonUntypedData[
+                              key as keyof TableHelper[TableDataNames]
+                            ]?.header}`}
+                          </Link>
+                        </TableCell>
+                      );
+                    }
                     return (
-                      <TableCell align="center" key={singleObject[key]}>
-                        {singleObject[key]}
+                      <TableCell sx={{ textAlign: 'center' }} key={key}>
+                        <Typography
+                          typography="p"
+                          textAlign="center"
+                          sx={{ color: 'red' }}
+                        >
+                          ERROR
+                        </Typography>
                       </TableCell>
                     );
-                  }
-
-                  return (
-                    <TableCell
-                      key={indexJSON}
-                      sx={{
-                        display: 'flex',
-                        justifyContent: 'center',
-                        gap: '1rem',
-                      }}
-                    >
-                      <Button
-                        variant="outlined"
-                        onClick={() => {
-                          redirectToEditPage(singleObject.id);
-                        }}
-                      >
-                        Edit
-                      </Button>
-                      <Button
-                        variant="outlined"
-                        color="error"
-                        onClick={() => {
-                          deleteHandler(singleObject.id);
-                        }}
-                      >
-                        DELETE <DeleteIcon />
-                      </Button>
-                    </TableCell>
-                  );
-                })}
-              </TableRow>
-            );
-          })}
-        </TableBody>
-      </Table>
-    </TableContainer>
+                  })}
+                </TableRow>
+              );
+            })}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </>
+  ) : (
+    <>
+      <Typography typography="h3" textAlign="center" sx={{ marginTop: '5rem' }}>
+        Add New Content :)
+      </Typography>
+      <ButtonGroup
+        variant="outlined"
+        size="large"
+        sx={{ marginTop: '5rem', textAlign: 'center' }}
+        fullWidth
+      >
+        <LinkButton
+          color="error"
+          component={Link}
+          to=".."
+          relative="path"
+          buttonText="Back to Home Page"
+        />
+        <LinkButton
+          component={Link}
+          to="new"
+          buttonText="ADD NEW"
+          state={{ urlKey: searchParams.get('key') }}
+        />
+      </ButtonGroup>
+    </>
   );
 }
 
